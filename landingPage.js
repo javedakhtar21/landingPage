cc4b92f61b1442a12ea973c32bb9b78669b9bca2
Md Javed Akhtar <mdjavedakhtar7323@gmail.com>
2024-04-01 00:37:23 +0530

    modification on all files

---
+++ landingPage.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menuBar = document.getElementById('menuBar');
+    menuBar.classList.toggle('active');
+}
+
+function closeMenu() {
+    var menuBar = document.getElementById('menuBar');
+    menuBar.classList.remove('active');
+}
